19ab424968e91df467c1c73ec40a1b63e51c47f5
Kevin Wall <kevinwall3186@gmail.com>
2023-06-03 18:20:02 -0400

    Radial Menu V2 Rotation
    - Rotation works between the arrow and the selector icon. The arrow follows the mouses position (when adjusted to follow screen width/height) and the selector rotates to the proper slice.

--- Assets/Scripts/RadialMenu_V2.cs
+++ Assets/Scripts/RadialMenu_V2.cs
@@ -1,7 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RadialMenu_V2 : MonoBehaviour
 {
@@ -10,15 +8,53 @@ public class RadialMenu_V2 : MonoBehaviour
     [SerializeField] GameObject radialUI;
     [Tooltip("The arrow to adjust to look at the mouse.")]
     [SerializeField] Transform arrow;
+    [Tooltip("The Player Stats UI to enable/disable when turning on/off the Radial Menu.")]
     [SerializeField] GameObject playerStatsUI;
+    [Tooltip("The Reticle UI to enable/disable when turning on/off the Radial Menu.")]
     [SerializeField] GameObject reticleUI;
+    [SerializeField] GameObject[] slices;
+    [Tooltip("A transparent selector to display which item is being hovered on.")]
+    [SerializeField] Transform selector;
 
+    #region ScriptVariables
+    /// <summary>
+    /// The angle between each slice in the radial menu.
+    /// </summary>
+    int sliceAng;
+    
+    /// <summary>
+    /// Keeps track of if the radial menu should be displayed or not.
+    /// </summary>
     bool isMenuBeingShown;
+    
+    /// <summary>
+    /// Keeps track of if the angle is above the minimum amount of degrees.
+    /// </summary>
+    bool withinRadialMin;
+    
+    /// <summary>
+    /// Keeps track of if the angle is below the minimum amount of degrees
+    /// Used in adjusting the selector image.
+    /// </summary>
+    bool withinRadialMax;
+    
+    /// <summary>
+    /// Keeps track of the Mouse's position. Used in adjusting the selector image.
+    /// </summary>
     Vector2 mousePos;
+    
+    /// <summary>
+    /// Keeps track of the rotation angle from the mouse to the middle.
+    /// Used in adjusting the selector and updating the arrow image
+    /// </summary>
     float rotateAngle;
+
+    #endregion
 
     private void Start()
     {
+        sliceAng = 360 / slices.Length;
+
         // On the first frame if the radial menu is left on
         // in the inspector, turn it off no matter what. So it doesn't show.
         // then when the player presses Q it will show up again in the update funciton.
@@ -36,6 +72,7 @@ public class RadialMenu_V2 : MonoBehaviour
         if (isMenuBeingShown)
         {
             UpdateMousePosition();
+            UpdateSelectedItem();
         }
     }
 
@@ -48,19 +85,36 @@ public class RadialMenu_V2 : MonoBehaviour
         playerStatsUI.SetActive(!isMenuBeingShown);
         reticleUI.SetActive(!isMenuBeingShown);
         radialUI.SetActive(isMenuBeingShown);
-
-        // Set the cursor to the opposite of the menu's visiblity
-        Cursor.visible = !isMenuBeingShown;
     }
 
     void UpdateMousePosition()
     {
-        mousePos = Input.mousePosition;
+        mousePos = Input.mousePosition - new Vector3(Screen.width / 2, Screen.height / 2);
+        
         rotateAngle = Vector2.SignedAngle(Vector2.up, mousePos);
-        //Debug.Log(rotateAngle);
+        rotateAngle = rotateAngle < 0 ? rotateAngle + 360 : rotateAngle;
         arrow.rotation = Quaternion.Euler(0, 0, rotateAngle);
 
-        // Continuous rotation of the angle by time. (Like a spinner)
-        // arrow.Rotate(Vector3.forward, rotateAngle * Time.deltaTime);
+    }
+
+    void UpdateSelectedItem()
+    {
+        for (int sliceIndex = 0; sliceIndex < slices.Length; ++sliceIndex)
+        {
+            // Checks to see if the current slice is within the minimum or maximum angles
+            // of the slice. 
+            withinRadialMin = rotateAngle > sliceIndex * sliceAng;
+            withinRadialMax = rotateAngle < (sliceIndex + 1) * sliceAng;
+
+            if (withinRadialMin && withinRadialMax)
+            {
+                selector.transform.rotation = Quaternion.Euler(0, 0, sliceIndex * sliceAng + (sliceAng * 3));
+            }
+        }
+    }
+
+    void SelectKinesis()
+    {
+
     }
 }
