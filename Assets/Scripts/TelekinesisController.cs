b97cff0b6b86e719742a5a73d19e523a161e20b4
tomOrourke1 <60354943+tomOrourke1@users.noreply.github.com>
2023-06-03 11:13:10 -0400

    Update working on the pulling of objects.

--- Assets/Scripts/TelekinesisController.cs
+++ Assets/Scripts/TelekinesisController.cs
@@ -7,17 +7,24 @@ public class TelekinesisController : MonoBehaviour
 
     [Header("----- Components -----")]
     [SerializeField] Camera cam;
+    [SerializeField] Transform desiredPos; 
 
     [Header("----- stats ------")]
     [SerializeField] float range;
     [SerializeField] int pullForce;
     [SerializeField] int pushForce;
 
+    [SerializeField] float yOffset;
+
+    [SerializeField][Range(0, 5)] float timeSpeed;
+
 
     bool isHoldingObject;
-
+    float timePressed;
 
     ITelekinesis stachedObject;
+
+    Vector3 originalPos;
 
 
     void Start()
@@ -26,10 +33,26 @@ public class TelekinesisController : MonoBehaviour
     }
 
 
-    void Update()
+    void FixedUpdate()
     {
-        
-        if(Input.GetKeyDown(KeyCode.Mouse0))
+
+        TelekinesisStart();
+
+        PullObject();
+
+        ReleaseObject();       
+
+
+
+    }
+
+    
+
+
+
+    void TelekinesisStart()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             // suck item
 
@@ -37,37 +60,61 @@ public class TelekinesisController : MonoBehaviour
             RaycastHit hit;
             bool doesHit = Physics.Raycast(ray, out hit, range);
 
-            if(doesHit)
+            if (doesHit)
             {
                 stachedObject = hit.collider.GetComponent<ITelekinesis>();
+                timePressed = 0;
+                if(stachedObject != null)
+                {
 
-
+                    originalPos = stachedObject.GetPosition();
+                    stachedObject.GetRigidbody().useGravity = false;
+                }
             }
-
-
         }
+    }
 
 
-        if(stachedObject != null)
+    void PullObject()
+    {
+        if (stachedObject != null)
         {
             var pos = stachedObject.GetPosition();
 
-            var dir = transform.position - pos;
+            var dir = desiredPos.position - pos;
+
+            var midPoint = pos + (dir * 0.5f);
 
             dir.Normalize();
 
+
+            midPoint += Vector3.up * yOffset;
+
+
+
+            Vector3 desPos = lerp2(originalPos, midPoint, desiredPos.position, Mathf.Clamp01(timePressed * timeSpeed));
+
+
+            Vector3 dirToDesPos = desPos - pos;
+            Debug.DrawLine(desPos, pos);
+
+            
+            //stachedObject.TakeVelocity(dirToDesPos);
+
             var rb = stachedObject.GetRigidbody();
+            rb.position = desPos;
 
-            rb.AddForce(dir * pullForce, ForceMode.Force);
-
-
+            timePressed += Time.deltaTime;
+            timePressed = Mathf.Clamp01(timePressed);
         }
+    }
 
-
-
-        if(Input.GetKeyUp(KeyCode.Mouse0) && stachedObject != null)
+    void ReleaseObject()
+    {
+        if (Input.GetKeyUp(KeyCode.Mouse0) && stachedObject != null)
         {
 
+            stachedObject.GetRigidbody().useGravity = true;
 
             // if it is being held than trow it.
             // otherwise just release it
@@ -77,15 +124,38 @@ public class TelekinesisController : MonoBehaviour
             stachedObject = null;
         }
 
-
-
-
     }
 
 
 
 
+    
+    Vector3 CalculateCubicBezierPoint(Vector3 p0, Vector3 p1, float t)
+    {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+        float uuu = uu * u;
+        float ttt = tt * t;
 
+        Vector3 p = uuu * p0;
+        p += 3 * uu * t * p1;
+        //p += 3 * u * tt * p2;
+        //p += ttt * p3;
+
+        return p;
+    }
+
+
+    Vector3 lerp2(Vector3 p0, Vector3 p1, Vector3 p2, float t)
+    {
+        Vector3 pp = Vector3.Lerp(p0, p1, t);
+        Vector3 ppp = Vector3.Lerp(p1, p2, t);
+
+        Vector3 pppp = Vector3.Lerp(pp, ppp, t);
+
+        return pppp;
+    }
 
 
 
