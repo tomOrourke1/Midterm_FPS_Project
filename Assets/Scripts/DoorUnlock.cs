13c20b075288d3b3c7a5cf5454e29c8dfb2dfabe
ZachBaritell <Maverick8371@gmail.com>
2023-06-07 15:13:36 -0400

    UI fixes

---
+++ Assets/Scripts/DoorUnlock.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class DoorUnlock : MonoBehaviour
+{
+    GameObject Key;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /*
+     Make a key game object
+    make it's collider a trigger
+    if the player touches it
+    unlock the referenced door
+    i don't know how to do that
+    but we will figure it out
+
+     */
+}
