f34ae58a7cd9e69aaec68d80e0e0fb8e0a7e112e
tomOrourke1 <60354943+tomOrourke1@users.noreply.github.com>
2023-06-06 23:09:26 -0400

    Worked on the targets for interactions. This includes the version for damage and the version for trigger enter.

---
+++ Assets/Scripts/CountedActivationScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CountedActivationScript : MonoBehaviour
+{
+
+
+    [SerializeField] int desiredCount;
+
+    [SerializeField] UnityEvent activationEvent;
+
+
+    int count;
+    bool activated;
+
+    private void Start()
+    {
+        activated = false;
+    }
+
+    public void Increment()
+    {
+        count++;
+
+        if(count >= desiredCount && !activated)
+        {
+            activationEvent?.Invoke();
+        }
+    }
+
+    public void Decrement() 
+    { 
+        count--; 
+
+        if(count < desiredCount)
+        {
+            activated = false;
+        }
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/TargetTriggerScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TargetTriggerScript : MonoBehaviour
+{
+
+    [SerializeField] UnityEvent targetEvent;
+
+    [SerializeField] Renderer targetRenderer;
+    [SerializeField] Material targeCheckedMaterial;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        targetEvent?.Invoke();
+        targetRenderer.material = targeCheckedMaterial;
+    }
+
+}
