8f17c42772a07d5105e1150716ef9b380c83320e
Jerry Banaag <JerryBanaag3@gmail.com>
2023-06-08 17:24:57 -0400

    Fixed player void damage script

--- Assets/Scripts/VoidDamage/PlayerVoidDamage.cs
+++ Assets/Scripts/VoidDamage/PlayerVoidDamage.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class PlayerVoidDamage : MonoBehaviour, IVoidDamage
 {
-    [SerializeField] Player player;
 
     public void FallIntoTheVoid()
     {
-        player.TakeDamage((int) player.GetPlayerMaxHP() + (int) player.GetPlayerMaxShield());
+        gameManager.instance.LoseGame();
     }
 }
