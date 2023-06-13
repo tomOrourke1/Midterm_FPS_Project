473498b1bc925675853de1ac2cf8fa0a5e44a8f5
tomOrourke1 <60354943+tomOrourke1@users.noreply.github.com>
2023-06-12 21:08:02 -0400

    Update InputManager.cs

--- Assets/Scripts/Input/InputManager.cs
+++ Assets/Scripts/Input/InputManager.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    // instance
     public static InputManager Instance;
 
     GameInput input;
 
-
+    // properties
     public GameInput Input => input;
     public GameInput.PlayerActions Action => input.Player;
 
@@ -23,11 +24,17 @@ public class InputManager : MonoBehaviour
     {
         input = new GameInput();
         input.Player.Enable();
+    }
 
+    private void OnEnable()
+    {
         input.Player.Escape.performed += OnEscape;
         input.Player.OpenRadialWheel.performed += OnRadMenu;
-
-
+    }
+    private void OnDisable()
+    {
+        input.Player.Escape.performed -= OnEscape;
+        input.Player.OpenRadialWheel.performed -= OnRadMenu;
     }
 
     private void OnRadMenu(UnityEngine.InputSystem.InputAction.CallbackContext context)
@@ -42,7 +49,10 @@ public class InputManager : MonoBehaviour
 
 
 
+    private void Update()
+    {
 
+    }
 
 
 }
