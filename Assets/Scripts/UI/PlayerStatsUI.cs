c4ab87a7c38a682c39f66538d0bc7e56934dfff5
Kevin Wall <kevinwall3186@gmail.com>
2023-06-14 01:18:38 -0400

    Stat bars slide on resource being used
    Updated the tutorial level to have the Game Manager and Input System Managers in there. Also got sliding on the UI to work as it will decrease and then also increase whenever you spend or gain your resource stats.

--- Assets/Scripts/UI/PlayerStatsUI.cs
+++ Assets/Scripts/UI/PlayerStatsUI.cs
@@ -1,19 +1,36 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerStatsUI : MonoBehaviour
 {
-    [Header("Player Stat Fillers")]
+    [Header("Player Stat Sliders")]
     [Tooltip("Focus image to alter.")]
     [SerializeField] Image focusSlider;
-    [Tooltip("Shield image to alter.")]    
+    [Tooltip("Shield image to alter.")]
     [SerializeField] Image shieldSlider;
     [Tooltip("Health image to alter.")]
     [SerializeField] Image healthSlider;
+
+    [Header("Faux Sliders")]
+    [Tooltip("Faux Focus image to alter.")]
+    [SerializeField] Image fauxFocusSlider;
+    [Tooltip("Faux Shield image to alter.")]
+    [SerializeField] Image fauxShieldSlider;
+    [Tooltip("Faux Health image to alter.")]
+    [SerializeField] Image fauxHealthSlider;
+
+    [Header("Icons")]
     [Tooltip("The health icon to enable when the shield is empty.")]
     [SerializeField] Image healthIcon;
     [Tooltip("The shield icon to enable when the shield is empty.")]
     [SerializeField] Image shieldIcon;
+
+    [Header("Timers")]
+    [SerializeField] float reducingSpeed;
+    private float targetHealth, targetFocus, targetShield;
+
 
     // C# has inherent private protection but defining it just to be safe
     PlayerResources instance;
@@ -24,28 +41,29 @@ public class PlayerStatsUI : MonoBehaviour
         UpdateValues();
     }
 
+    // Updates the sliders constantly. There can be a different way to check this. LMK - Kev
+    private void Update()
+    {
+        UpdateSliders();
+    }
+
     // When the damage and HP refilling are added introduce these into those functions. 
     public void UpdateValues()
     {
-        if(instance == null)
+        if (instance == null)
         {
             instance = GameManager.instance.GetPlayerResources();
         }
+
+        targetFocus = instance.Focus.GetPercent();
+        targetShield = instance.Shield.GetPercent();
+        targetHealth = instance.Health.GetPercent();
 
         focusSlider.fillAmount = instance.Focus.GetPercent();
         shieldSlider.fillAmount = instance.Shield.GetPercent();
         healthSlider.fillAmount = instance.Health.GetPercent();
 
-        if (instance.Shield.CurrentValue > 0)
-        {
-            EnableShieldIcon();
-            DisableHealthIcon();
-        }
-        else if (instance.Health.CurrentValue > 0 && instance.Shield.CurrentValue == 0)
-        {
-            EnableHealthIcon();
-            DisableShieldIcon();
-        }
+        UpdateIcons();
     }
 
     /// <summary>
@@ -78,5 +96,32 @@ public class PlayerStatsUI : MonoBehaviour
     private void DisableShieldIcon()
     {
         shieldIcon.enabled = false;
+    }
+
+    /// <summary>
+    /// Updates the icons below the stat bars on the UI.
+    /// </summary>
+    private void UpdateIcons()
+    {
+        if (instance.Shield.CurrentValue > 0)
+        {
+            EnableShieldIcon();
+            DisableHealthIcon();
+        }
+        else if (instance.Health.CurrentValue > 0 && instance.Shield.CurrentValue == 0)
+        {
+            DisableShieldIcon();
+            EnableHealthIcon();
+        }
+    }
+
+    /// <summary>
+    /// Updates the faux sliders to match them with the real sliders.
+    /// </summary>
+    private void UpdateSliders()
+    {
+        fauxFocusSlider.fillAmount = Mathf.MoveTowards(fauxFocusSlider.fillAmount, targetFocus, reducingSpeed * Time.deltaTime);
+        fauxHealthSlider.fillAmount = Mathf.MoveTowards(fauxHealthSlider.fillAmount, targetHealth, reducingSpeed * Time.deltaTime);
+        fauxShieldSlider.fillAmount = Mathf.MoveTowards(fauxShieldSlider.fillAmount, targetShield, reducingSpeed * Time.deltaTime);
     }
 }
