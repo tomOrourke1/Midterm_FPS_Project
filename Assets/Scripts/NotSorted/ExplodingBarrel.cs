e85d09edbe0bbd11b62890f5f134b98333613a38
Jerry Banaag <JerryBanaag3@gmail.com>
2023-06-14 18:51:07 -0400

    Exploding barrels work

--- Assets/Scripts/NotSorted/ExplodingBarrel.cs
+++ Assets/Scripts/NotSorted/ExplodingBarrel.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class ExplodingBarrel : MonoBehaviour, IDamagable, IEntity
 {
+    [SerializeField] ParticleSystem Fuse;
     [SerializeField] int Durability;
     [SerializeField] float ExplosionTimer;
     [SerializeField] float ExplosionRange;
     [SerializeField] float ExplosionDamage;
-
-    List<IDamagable> victims;
 
     public void TakeDamage(float dmg)
     {
@@ -17,21 +16,47 @@ public class ExplodingBarrel : MonoBehaviour, IDamagable, IEntity
 
         if (Durability <= 0 )
         {
-            Timer();
+            StartCoroutine(Timer());
         }
     }
 
-    void Timer()
+    IEnumerator Timer()
     {
+        Fuse.Play();
         // Wait for timer
+        yield return new WaitForSeconds(ExplosionTimer);
         Explode();
     }
 
     void Explode()
     {
-        // Physics.SphereOverlay for damage
+        // Gets everything within range
         Collider[] colliders = Physics.OverlapSphere(transform.position, ExplosionRange);
 
+        // Grabs everything that was hit and checks if they are damagable
+        foreach (Collider collider in colliders)
+        {
+            if (collider.GetComponent<IDamagable>() != null)
+            {
+                RaycastHit hit;
+
+                // Gets the direction of the collider from the barrel.
+                var dir = collider.transform.position - transform.position;
+                //Debug.DrawRay(transform.position, dir);
+
+                // Casts from the barrel to the collider
+                Physics.Raycast(transform.position, dir, out hit);
+
+                // Damages the collider if the raycast connected with it.
+                if (hit.collider == collider)
+                {
+                    collider.GetComponent<IDamagable>().TakeDamage(ExplosionDamage);
+                }
+            }
+        }
+
+        // Play Explosion effects
+        Effects();
 
         // Delete Barrel
         Destroy(gameObject);
@@ -40,5 +65,10 @@ public class ExplodingBarrel : MonoBehaviour, IDamagable, IEntity
     public void Respawn()
     {
         Destroy(gameObject);
+    }
+
+    void Effects()
+    {
+
     }
 }
