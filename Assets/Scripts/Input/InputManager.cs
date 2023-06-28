cef7dc4862908b0167e58d81ecf23e1e6b08c64e
Kevin Wall <kevinwall3186@gmail.com>
2023-06-28 10:37:36 -0400

    Added Name and Company logo in build

--- Assets/Scripts/Input/InputManager.cs
+++ Assets/Scripts/Input/InputManager.cs
@@ -64,7 +64,7 @@ public class InputManager : MonoBehaviour
 
     private void OnInteract(UnityEngine.InputSystem.InputAction.CallbackContext context)
     {
-        UIManager.instance.uiStateMachine.SetInfo(true);
+        UIManager.instance.uiStateMachine.SetPlay(true);
         //if (UIManager.instance.currentState == MenuState.infographic)
         //{
         //    UIManager.instance.CloseInfoUI();
