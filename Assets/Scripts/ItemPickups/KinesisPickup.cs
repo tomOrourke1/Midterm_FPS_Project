ee350e102dc7a473fe654fb2dd288a0eb645c27f
tomOrourke1 <60354943+tomOrourke1@users.noreply.github.com>
2023-07-05 03:10:44 -0400

    I hope this doesn't break anything. Cause unity crashed and tried to break some stuff. And I don't know if it was fixed fully.

--- Assets/Scripts/ItemPickups/KinesisPickup.cs
+++ Assets/Scripts/ItemPickups/KinesisPickup.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using UnityEditor.Sprites;
 using UnityEngine;
 
 [System.Serializable]
--- Assets/Scripts/UI/Non_States/SettingsManager.cs
+++ Assets/Scripts/UI/Non_States/SettingsManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
