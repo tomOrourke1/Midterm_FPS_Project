e65b3cd321027572b3ddb0fd6683822d865f11fd
Kevin Wall <kevinwall3186@gmail.com>
2023-07-07 13:57:40 -0400

    Flash Damage UI more UX friendly

--- Assets/Scripts/UI/Non_States/FlashDamage.cs
+++ Assets/Scripts/UI/Non_States/FlashDamage.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class FlashDamage : MonoBehaviour
-{    
+{
     [Header("Images")]
     [SerializeField] Image flashDamage;
     [SerializeField] Image flashShield;
-    [SerializeField] Image screenCrack;
+    [SerializeField] Image flashCrack;
 
     [Header("Images")]
     [SerializeField] AnimationCurve flashSpeedCurve;
     [SerializeField] float showDuration;
 
-    private float tempShowDuration;
+    private float tmpFlashHP;
+    private float tmpFlashSH;
+    private float tmpFlashCR;
+
+    private bool currentlyFlashingDamage;
+    private bool currentlyFlashingShield;
+    private bool currentlyFlashingCrack;
 
     // =================================================================
     //          PLEASE DON'T TOUCH ANY OF THE WHILE LOOPS!
@@ -23,64 +29,123 @@ public class FlashDamage : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        tempShowDuration = showDuration;
+        tmpFlashHP = showDuration;
+        tmpFlashSH = showDuration;
+        tmpFlashCR = showDuration;
 
         flashDamage.gameObject.SetActive(false);
         flashShield.gameObject.SetActive(false);
-        screenCrack.gameObject.SetActive(false);
+        flashCrack.gameObject.SetActive(false);
     }
 
     public IEnumerator FlashDamageDisplay()
     {
-        StartCoroutine(CrackNShow(flashDamage, flashDamage.color.r, flashDamage.color.g, flashDamage.color.b, flashDamage.color.a));
-        yield return 0;
+        if (!currentlyFlashingDamage)
+        {
+            currentlyFlashingDamage = true;
+
+            flashDamage.gameObject.SetActive(true);
+
+            // While the timer is above a 'second'
+            while (tmpFlashHP > 0f)
+            {
+                // Add the Time.deltatime (interval in seconds from last frame to current frame) to the timer
+                tmpFlashHP -= Time.deltaTime;
+                // Evaluate the curve and then set that the alpha's amount
+                float alphaColorFadeIn = flashSpeedCurve.Evaluate(tmpFlashHP);
+                // Set the image color component to a new color of an decreased alpha
+                flashDamage.color = new Color(flashDamage.color.r, flashDamage.color.g, flashDamage.color.b, alphaColorFadeIn);
+                yield return 0;
+            }
+
+            tmpFlashHP = showDuration;
+            flashDamage.gameObject.SetActive(false);
+            currentlyFlashingDamage = false;
+        }
+        else
+        {
+            tmpFlashHP += Time.deltaTime;
+        }
     }
 
     public IEnumerator FlashShieldDisplay()
     {
-        StartCoroutine(CrackNShow(flashShield, flashShield.color.r, flashShield.color.g, flashShield.color.b, flashShield.color.a));;
-        yield return 0;
+        if (!currentlyFlashingShield)
+        {
+            currentlyFlashingShield = true;
+
+            flashShield.gameObject.SetActive(true);
+
+            // While the timer is above a 'second'
+            while (tmpFlashSH > 0f)
+            {
+                // Add the Time.deltatime (interval in seconds from last frame to current frame) to the timer
+                tmpFlashSH -= Time.deltaTime;
+                // Evaluate the curve and then set that the alpha's amount
+                float alphaColorFadeIn = flashSpeedCurve.Evaluate(tmpFlashSH);
+                // Set the image color component to a new color of an decreased alpha
+                flashShield.color = new Color(flashShield.color.r, flashShield.color.g, flashShield.color.b, alphaColorFadeIn);
+                yield return 0;
+            }
+
+            tmpFlashSH = showDuration;
+            flashShield.gameObject.SetActive(false);
+            currentlyFlashingShield = false;
+        }
+        else
+        {
+            tmpFlashSH += Time.deltaTime;
+        }
     }
 
     public IEnumerator CrackShieldDisplay()
     {
-        StartCoroutine(CrackNShow(screenCrack, screenCrack.color.r, screenCrack.color.g, screenCrack.color.b, screenCrack.color.a));
-        yield return 0;
-
-
-        //flashShield.gameObject.SetActive(true);
-        //screenCrack.gameObject.SetActive(true);
-        //yield return new WaitForSeconds(0.2f);
-
-        //float fadeOutCrack = 0;
-        //while (fadeOutCrack <= 0.5f)
-        //{
-        //    fadeOutCrack += Time.deltaTime;
-        //    screenCrack.color = new Color(0f, 0f, 0f, fadeOutCrack / 0.5f);
-        //}
-        //flashShield.gameObject.SetActive(false);
-        //screenCrack.gameObject.SetActive(false);
-        //screenCrack.color = new Color(255f, 255f, 255f, 255f);
-    }
-
-    private IEnumerator CrackNShow(Image image, float r, float g, float b, float a)
-    {
-        image.gameObject.SetActive(true);
-        image.color = new Color(r, g, b, a);
-
-        // While the timer is above a 'second'
-        while (showDuration > 0f)
+        if (!currentlyFlashingCrack)
         {
-            // Add the Time.deltatime (interval in seconds from last frame to current frame) to the timer
-            showDuration -= Time.deltaTime;
-            // Evaluate the curve and then set that the alpha's amount
-            float alphaColorFadeIn = flashSpeedCurve.Evaluate(showDuration);
-            // Set the image color component to a new color of an decreased alpha
-            image.color = new Color(r, g, b, alphaColorFadeIn);
-            yield return 0;
-        }
+            currentlyFlashingCrack = true;
 
-        showDuration = tempShowDuration;
-        image.gameObject.SetActive(false);
+            flashCrack.gameObject.SetActive(true);
+
+            // While the timer is above a 'second'
+            while (tmpFlashSH > 0f)
+            {
+                // Add the Time.deltatime (interval in seconds from last frame to current frame) to the timer
+                tmpFlashCR -= Time.deltaTime;
+                // Evaluate the curve and then set that the alpha's amount
+                float alphaColorFadeIn = flashSpeedCurve.Evaluate(tmpFlashCR);
+                // Set the image color component to a new color of an decreased alpha
+                flashCrack.color = new Color(flashCrack.color.r, flashShield.color.g, flashDamage.color.b, alphaColorFadeIn);
+                yield return 0;
+            }
+
+            tmpFlashCR = showDuration;
+            flashCrack.gameObject.SetActive(false);
+            currentlyFlashingCrack = false;
+        }
+        else
+        {
+            tmpFlashCR += Time.deltaTime;
+        }
     }
+
+    //private IEnumerator CrackNShow(Image image, float r, float g, float b, float a)
+    //{
+    //    image.gameObject.SetActive(true);
+    //    image.color = new Color(r, g, b, a);
+
+    //    // While the timer is above a 'second'
+    //    while (showDuration > 0f)
+    //    {
+    //        // Add the Time.deltatime (interval in seconds from last frame to current frame) to the timer
+    //        showDuration -= Time.deltaTime;
+    //        // Evaluate the curve and then set that the alpha's amount
+    //        float alphaColorFadeIn = flashSpeedCurve.Evaluate(showDuration);
+    //        // Set the image color component to a new color of an decreased alpha
+    //        image.color = new Color(r, g, b, alphaColorFadeIn);
+    //        yield return 0;
+    //    }
+
+    //    showDuration = tempShowDuration;
+    //    image.gameObject.SetActive(false);
+    //}
 }
