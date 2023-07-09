ec0dd69ab47639e8bfc709ab60bd281afa5012ec
Kevin Wall <kevinwall3186@gmail.com>
2023-07-09 01:48:31 -0400

    Taking out Usings in Area manager that are unneeded

--- Assets/Scripts/AreaManager/AreaManager/AreaManager.cs
+++ Assets/Scripts/AreaManager/AreaManager/AreaManager.cs
@@ -1,9 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEngine.EventSystems.EventTrigger;
 
 public class AreaManager : MonoBehaviour
 {
