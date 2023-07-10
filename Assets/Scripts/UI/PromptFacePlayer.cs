b83385e3c90a5942b800a99b5855e44fdb0cd314
Kevin Wall <kevinwall3186@gmail.com>
2023-07-10 13:26:42 -0400

    Updated Prompted.obj prefab

--- Assets/Scripts/UI/PromptFacePlayer.cs
+++ Assets/Scripts/UI/PromptFacePlayer.cs
@@ -12,13 +12,41 @@ public class PromptFacePlayer : MonoBehaviour
         dir.Normalize();
 
         transform.localRotation = Quaternion.LookRotation(-dir);
-
     }
 
+    [Header("Popup Components")]
+    [SerializeField] GameObject keyboardObj;
+    [SerializeField] GameObject controllerObj;
 
+    private void Start()
+    {
+        DisplayInteractText();
+    }
 
+    /// <summary>
+    /// From the IPopup interface, sets the active game object to display how to interact to being hidden.
+    /// </summary>
+    public void DisplayInteractText()
+    {
+        if (InputManager.Instance.GamepadActive)
+        {
+            DisplayController();
+        }
+        else if (!InputManager.Instance.GamepadActive)
+        {
+            DisplayKeyboard();
+        }
+    }
 
+    private void DisplayController()
+    {
+        controllerObj?.SetActive(true);
+        keyboardObj?.SetActive(false);
+    }
 
-
-
+    private void DisplayKeyboard()
+    {
+        controllerObj?.SetActive(false);
+        keyboardObj?.SetActive(true);
+    }
 }
